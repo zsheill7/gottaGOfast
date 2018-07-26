58460452fc34f8cf5be1f9313ec9e31b9313aef7
Zoe Sheill <zsheill7@gmail.com>
2018-07-25 17:51:35 -0700

    first commit

---
+++ helloworld/main.go
@@ -0,0 +1,19 @@
+package fjkdlsa
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hi there!")
+}
+
+// How do we run code inside our project?
+
+// What does package main mean?
+// First line must indicate which package it belongs to
+// Two types: executable and reusable.  Reusable code is used as helpers. Can use it for different things
+
+// What does import fmt mean?
+
+// What does func mean?
+
+// How is main.go file organized?
